dfef999a70025ea29595f6dd1d4e56f503096c67
Viktor Rura <ruravictor@gmail.com>
2024-10-05 18:33:27 +0100

    DB fix check

--- django_social_auth/settings.py
+++ django_social_auth/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         "NAME": "d9lfsgvpoif54m",
         "USER": "ubiu7cm68up1pd",
         "PASSWORD": "pb9750afb38ad89e41b11cf5dd8e7dc7241c1b88584e7e59599a55e616bbae0b2",
-        "HOST": "pb9750afb38ad89e41b11cf5dd8e7dc7241c1b88584e7e59599a55e616bbae0b2@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",
+        "HOST": "c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",
         "PORT": "5432",
     },
 }
