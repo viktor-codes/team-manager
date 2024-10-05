e26170523556b4cd6da3e1ddcae9bd0b8229a59d
Viktor Rura <ruravictor@gmail.com>
2024-10-05 15:12:35 +0100

    Fix the heroku host again

--- django_social_auth/settings.py
+++ django_social_auth/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "team-manager-05f5965c2315.herokuapp.com"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "team-manager-05f5965c2315.herokuapp.com",
+    ".herokuapp.com",
+]
 
 
 # Application definition
