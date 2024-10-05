01b3c4c30be642542e1bf3977b2243cb6882ac9f
Viktor Rura <ruravictor@gmail.com>
2024-10-05 18:19:35 +0100

    Add correct engine value to db settings

--- django_social_auth/settings.py
+++ django_social_auth/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = "django_social_auth.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
