9782b1cc04f1ed942b1d8ee91e4f2c1eabc88a08
Viktor Rura <ruravictor@gmail.com>
2024-10-05 18:17:53 +0100

    Fix DB var names

--- django_social_auth/settings.py
+++ django_social_auth/settings.py
@@ -98,12 +98,12 @@ WSGI_APPLICATION = "django_social_auth.wsgi.application"
 
 DATABASES = {
     "default": {
-        "DB_ENGINE": os.getenv("DB_ENGINE"),
-        "DB_NAME": os.getenv("DB_NAME"),
-        "DB_USER": os.getenv("DB_USER"),
-        "DB_PASSWORD": os.getenv("DB_PASSWORD"),
-        "DB_HOST": os.getenv("DB_HOST"),
-        "DB_PORT": os.getenv("DB_PORT"),
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     },
 }
 
