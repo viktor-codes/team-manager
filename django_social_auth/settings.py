e78d4ced35cf10f92fea7db3f09ad85448cc49b6
Viktor Rura <ruravictor@gmail.com>
2024-10-05 14:58:16 +0100

    Add allowed hosts

--- django_social_auth/settings.py
+++ django_social_auth/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "https://team-manager-05f5965c2315.herokuapp.com/",
+]
 
 
 # Application definition
